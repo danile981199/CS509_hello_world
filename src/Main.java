8834f3844f4550d9906291a1c42c5d08b5619214
danile981199 <dcla0090@gmail.com>
2025-04-15 21:32:56 -0400

    Initial commit: Hello World Java Project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main class for Hello World.
+ */
+public class Main {
+    /**
+     * Entry point.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
